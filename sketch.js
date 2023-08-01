bf33b7f86bb0bd8fc44cfc2b9593597a993d7013
Ayushi Jain <j.ayushi0903@gmail.com>
2023-08-01 16:06:27 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+
+
+function preload(){
+  
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  
+// Moving background
+
+
+//creating boy running
+
+  
+// create left Boundary
+
+//create right Boundary
+
+}
+function draw() {
+ 
+  
+  // boy moving on Xaxis with mouse
+ 
+  
+  //code to reset the background
+  
+  drawSprites();
+}
